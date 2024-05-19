0fffdba4640496eac11ffae12cea4d6004e9035e
Ignacio M <carlos@openix.com.ar>
2024-05-18 22:45:27 -0300

    add products outlet

--- src/components/OutletProduct/OutletProduct.tsx
+++ src/components/OutletProduct/OutletProduct.tsx
@@ -1,79 +1,37 @@
+import { useProductStore } from "@/store/Products";
 import React from "react";
+import { OutletProductCard } from "../OutletProductCard/OutletProductCard";
 
 export const OutletProduct = () => {
+  const products = useProductStore((state) => state.products);
+  const outletProducts = products
+
+    .filter((product) => product.outlet)
+    .slice(0, 3);
   return (
-    <div className="bg-gray-100 md:px-10 px-4 py-12 font-[sans-serif]">
-      <div className="container mx-auto">
-        <h2 className="text-3xl font-extrabold text-gray-800 mb-8">
-          Latest Blog Posts
-        </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          <div className="bg-white rounded-lg overflow-hidden shadow-lg">
-            <img
-              src="https://readymadeui.com/cardImg.webp"
-              alt="Blog Post 1"
-              className="w-full h-52 object-cover"
-            />
-            <div className="p-6">
-              <h3 className="text-xl font-bold text-gray-800 mb-2">
-                Lorem Ipsum Dolor
-              </h3>
-              <p className="text-gray-600 text-sm">
-                Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore...
-              </p>
-              <a
-                href="javascript:void(0);"
-                className="mt-4 inline-block text-blue-600 text-sm hover:underline"
-              >
-                Read More
-              </a>
-            </div>
-          </div>
-          <div className="bg-white rounded-lg overflow-hidden shadow-lg">
-            <img
-              src="https://readymadeui.com/hotel-img.webp"
-              alt="Blog Post 2"
-              className="w-full h-52 object-cover"
-            />
-            <div className="p-6">
-              <h3 className="text-xl font-bold text-gray-800 mb-2">
-                Consectetur Adipiscing
-              </h3>
-              <p className="text-gray-600 text-sm">
-                Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore...
-              </p>
-              <a
-                href="javascript:void(0);"
-                className="mt-4 inline-block text-blue-600 text-sm hover:underline"
-              >
-                Read More
-              </a>
-            </div>
-          </div>
-          <div className="bg-white rounded-lg overflow-hidden shadow-lg">
-            <img
-              src="https://readymadeui.com/team-image.webp"
-              alt="Blog Post 3"
-              className="w-full h-52 object-cover"
-            />
-            <div className="p-6">
-              <h3 className="text-xl font-bold text-gray-800 mb-2">
-                Lorem Ipsum Sit Amet
-              </h3>
-              <p className="text-gray-600 text-sm">
-                Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore...
-              </p>
-              <a
-                href="javascript:void(0);"
-                className="mt-4 inline-block text-blue-600 text-sm hover:underline"
-              >
-                Read More
-              </a>
-            </div>
-          </div>
+    // <div classNameName="bg-gray-100 md:px-10 px-4 py-12 font-[sans-serif]">
+    //   <div classNameName="container mx-auto">
+    //     <h2 classNameName="text-3xl font-extrabold text-gray-800 mb-8">
+    //       Productos destacados
+    //     </h2>
+    //     <div classNameName="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    //       {outletProducts.map((product) => (
+    //         <OutletProductCard key={product.id} product={product} />
+    //       ))}
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="bg-white font-[sans-serif] my-4">
+      <div className="max-w-7xl mx-auto">
+        <div className="text-center">
+          <h2 className="text-3xl font-extrabold text-[#333] inline-block relative after:absolute after:w-4/6 after:h-1 after:left-0 after:right-0 after:-bottom-4 after:mx-auto after:bg-pink-400 after:rounded-full">
+            Productos destacados
+          </h2>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-16 max-md:max-w-lg mx-auto">
+          {outletProducts.map((product) => (
+            <OutletProductCard key={product.id} product={product} />
+          ))}
         </div>
       </div>
     </div>
---
+++ src/components/OutletProductCard/OutletProductCard.tsx
@@ -0,0 +1,36 @@
+import { Product } from "@/types/ProductType";
+import Image from "next/image";
+import Link from "next/link";
+
+type OutletProductCardProps = {
+  product: Product;
+};
+export const OutletProductCard = ({ product }: OutletProductCardProps) => {
+  return (
+    <div className="bg-white cursor-pointer rounded overflow-hidden shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative top-0 hover:-top-2 transition-all duration-300">
+      <Link href={`/products/${product.id}`}>
+        <Image
+          src={product.image}
+          alt="Blog Post 1"
+          className="w-full h-60 object-cover"
+          width={400}
+          height={400}
+        />
+        <div className="p-6">
+          <span className="text-sm block text-gray-400 mb-2">
+            {product.createdAt
+              ? // ? new Intl.DateTimeFormat("en-US", { dateStyle: "medium" }).format(
+                //     product.createdAt
+                //   )
+                "-"
+              : "-"}
+            | {product.category}
+          </span>
+          <h3 className="text-xl font-bold text-[#333]">{product.title}</h3>
+          <hr className="my-6" />
+          <p className="text-gray-400 text-sm">{product.description}</p>
+        </div>
+      </Link>
+    </div>
+  );
+};
